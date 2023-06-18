37f5f3b2376e5cf4da9456ef3952b6ef44de90a1
Vokturz <victornavarrrokp47@gmail.com>
2023-06-17 21:19:46 -0400

    huge changes, considering removing handlers.py...

--- src/handlers.py
+++ src/handlers.py
@@ -1,45 +1,192 @@
 from . import prompts
-from langchain.llms import GPT4All, OpenAI
+from langchain.llms import (OpenAI, CTransformers, FakeListLLM)
+from langchain.chat_models import ChatOpenAI
 import time
 from pathlib import Path
 import requests
+import tiktoken
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.embeddings import (HuggingFaceEmbeddings, FakeEmbeddings)
+from langchain.vectorstores import Chroma
+from langchain.prompts import PromptTemplate
+import json
+import os
+import re
+# Get the directory path of the current script
+current_directory = os.path.dirname(os.path.abspath(__file__))
+files_path = "files"
 
-model = '/mnt/coldstorage/vnavarro/models/ggml-vicuna-13b-1.1-q4_2.bin'
-files_path = 'files'
+def parse_format_query(query, bot_user_id=None):
+    to_all = False
+    change_temp = False
+    if '!all' in query:
+        query = query.replace('!all', '').strip()
+        to_all = True
+    new_temp = re.search(r'!temp=([\d.]+)', query)
+    if new_temp:
+        try:
+            new_temp = float(new_temp.group(1))
+            assert 0<=new_temp<=1
+            change_temp = True
+        except:
+            new_temp = -1
+            change_temp = False
+        query = re.sub(r'!temp=([\d.]+)', '', query).strip()
 
-def get_llm(model_type):
-    if model_type == 'GPT4All':
-        llm = GPT4All(model=model, temp=0, n_predict=128, top_p=0.95, n_threads=20)
-    elif model_type == 'OpenAI':
-        llm =  OpenAI(temperature=0, max_tokens=128, top_p=0.95)
-    return llm
+    if bot_user_id:
+        query = query.replace(f'<@{bot_user_id}>', '')
+    res = {'query' : query,
+           'new_temp' : new_temp,
+           'to_all' : to_all,
+           'change_temp' : change_temp}
+    return res
 
-def create_handlers(bot, model_type='GPT4All', show_time=False):
+def get_ask_classifier(embeddings_clf, phrase):
+    search_index = Chroma.from_texts(texts=['opinion', 'coding'], embedding=embeddings_clf)
+    res = search_index.similarity_search_with_score(phrase, k=2)
+    classif = res[0][0].page_content
+    score_diff = res[1][1] - res[0][1]
+    if score_diff < 0.1:
+        return 'opinion'
+    return classif
+
+def get_llm_reply(bot, say, prompt, query, to_all=False, thread_ts=None):
+    #embd_clf = bot.get_embeddings()[1]
+    #classif = get_ask_classifier(embd_clf, query)
+    llm = bot.get_llm()
+
+    if thread_ts: # format only if is not a thread
+        final_prompt = prompt
+    else:
+        final_prompt = prompt.format(query=query)
+
+    n_tokens =  llm.get_num_tokens(final_prompt)
+    start_time = time.time()
+    if to_all:
+        init_msg = "The bot is thinking.. :hourglass_flowing_sand:"
+        if bot.get_verbose():
+            init_msg += f"(_`temperature={bot.get_temperature()}, n_tokens={n_tokens}`_)"
+            #init_msg += f". Your question was classified as *{classif}*_)"
+        initial_message = say(init_msg, thread_ts=thread_ts)
+    else:
+        initial_message = None
+
+    resp_llm = llm(final_prompt).strip()
+    
+    final_time = round((time.time() - start_time)/60,2)
+    response = resp_llm
+    if bot.get_verbose():
+        response += f"\n (_time: `{final_time}` min. `temperature={bot.get_temperature()}, n_tokens={n_tokens}`_)"
+        #response += f" Your question was classified as *{classif}*_)"
+    return response, initial_message
+    
+
+
+
+def create_handlers(bot):
     @bot.command("/ask")
-    def ask(ack, respond, command, say):
+    def ask(ack, respond, command, say, client):
         user = command["user_id"]
+        channel_id = command['channel_id']
         query = command["text"]
-        ack()
-        prompt = prompts.DEFAULT_PROMPT
-        llm = get_llm(model_type)
-        to_all = False
-        if query.startswith('all'):
-            query = query.replace('all ', '')
-            to_all = True
-        start_time = time.time()
-        resp_llm = llm(prompt.format(query=query))
-        final_time = round((time.time() - start_time)/60,2)
+        channel_llm_info = bot.get_channel_llm_info(channel_id)
+        actual_temp = channel_llm_info['temperature']
+        bot.change_temperature(actual_temp)
 
-        response = f"\n*Answer*: {resp_llm}"
-        if show_time:
-            response += f"\n (_time: `{final_time}` min_)"
-        if to_all:
-            response = f"*<@{user}> asked*: {query}" + response
-            say(response)
+        prompt = PromptTemplate(template=prompts.DEFAULT_PROMPT,
+                                input_variables=['personality',
+                                                 'instructions',
+                                                 'query'])
+        channel_prompt = prompt.partial(personality=channel_llm_info['personality'],
+                                        instructions=channel_llm_info['instructions'])
+        parsed_query = parse_format_query(query)
+        
+        if parsed_query['change_temp']:
+                bot.change_temperature(temperature=parsed_query['new_temp'])
+        if parsed_query['new_temp'] == -1:
+                respond(f"`!temp` only accepts values between 0 and 1."
+                        f" Using current value of `{actual_temp}`")
+        ack()
+        response, initial_message = get_llm_reply(bot, say, channel_prompt,
+                                                  parsed_query['query'],
+                                                  to_all=parsed_query['to_all'])
+        
+        response = f"*<@{user}> asked*: {parsed_query['query']}\n*Answer*:\n{response}"
+        if parsed_query['to_all']:
+            client.chat_update(
+                channel=channel_id,
+                ts=initial_message['ts'],
+                text=response
+            )
         else:
-            response = f"*You asked*: {query}" + response
             respond(response)
+        # back to default temperature
+        bot.change_temperature(temperature=actual_temp)
+
+
+    @bot.command("/modify_bot")
+    def modify_bot(ack, body, client, logger, respond):
+        ack()
+        channel_id = body['channel_id']
+        trigger_id = body['trigger_id']
+        if channel_id[0] not in ['C', 'G']:
+            respond(text='This command can only be used in channels.')
+            return
+        
+        with open(f'{current_directory}/payloads/modify_bot_template.json', 'r') as f:
+            template = f.read()
+        
+        channel_bot_info = bot.get_channel_llm_info(channel_id)
             
+        
+        template = template.format(personality=channel_bot_info['personality'],
+                                instructions=channel_bot_info['instructions'],
+                                temperature=channel_bot_info['temperature'])
+        view = json.loads(template)
+
+        # include channel_id
+        view["private_metadata"] =  json.dumps({"channel_id": channel_id})
+        client.views_open(trigger_id=trigger_id, view=view)
+        
+    @bot.view('modify_bot')
+    def handle_modify_bot(ack, body, logger, say, view):
+        values = view['state']['values']
+        bot_values = {'personality': '',
+                      'instructions': '',
+                      'temperature' : ''
+                      }
+        channel_id = json.loads(view["private_metadata"])["channel_id"]
+        user = body['user']['id']
+        for key in bot_values.keys():
+            input_value = values[key][key]['value']
+            if key == 'temperature':
+                try:
+                    input_value = float(input_value)
+                    assert 0<=input_value<=1
+                except:
+                    ack({"response_action": "errors",
+                        "errors": {key: "The input field must be a number between 0 and 1."}})
+                    return        
+            bot_values[key] = input_value
+        bot.define_channel_llm_info(channel_id, bot_values)
+        ack()
+        say(f'<@{user}> has modified the bot info', channel=channel_id)
+
+    @bot.command("/bot_info")
+    def get_bot_info(ack, respond, command):
+        ack()
+        channel_id = command['channel_id']
+        if channel_id[0] not in ['C', 'G']:
+            respond(text='This command can only be used in channels.')
+            return
+        bot_info = bot.get_channel_llm_info(channel_id)
+        prompt = prompts.INITIAL_BOT_PROMPT
+        res = "*Default Prompt:*\n`"
+        res += prompt.format(personality=bot_info["personality"],
+                              instructions=bot_info["instructions"])
+        res += f"`\n*Temperature:* {bot_info['temperature']}"
+        respond(res)
+
     # @bot.event("file_shared")
     # def file_func(payload, client, ack):
     #     ack()
@@ -125,12 +272,62 @@ def create_handlers(bot, model_type='GPT4All', show_time=False):
     #     )
 
     @bot.event("app_mention")
-    def handle_mention(body, say):
-        user = body["event"]["user"]
-        text = body["event"]["text"]
-        say(f"Hey, <@{user}>!")
+    def handle_mention(body, say, logger, client, respond):
+        channel_id = body['event']['channel']
+        bot_user_id = client.auth_test()['user_id']
+        user_query = body['event']['text'].replace(f'<@{bot_user_id}>', '')
+        channel_llm_info = bot.get_channel_llm_info(channel_id)
+        actual_temp = channel_llm_info['temperature']
+        bot.change_temperature(actual_temp)
+        try:
+            thread_ts = body['event']['thread_ts']
+        except:
+            thread_ts = None
+        if thread_ts:
+            try:
+                messages_history, users = bot.extract_thread_conversation(channel_id, thread_ts)
 
-    @bot.event("message")
-    def handle_message(body, say):
-        return
+                prompt = PromptTemplate(template=prompts.THREAD_PROMPT,
+                                        input_variables=['personality',
+                                                         'instructions',
+                                                         'users',
+                                                         'conversation'])
+                
+                final_prompt = prompt.format(personality=channel_llm_info['personality'],
+                                             instructions=channel_llm_info['instructions'],
+                                             users=' '.join(list(users)),
+                                             conversation='\n'.join(messages_history))
+                
+                parsed_query = parse_format_query(user_query, bot_user_id=bot_user_id)
+
+                # check if user ask for change the temperature
+                if parsed_query['change_temp']:
+                        bot.change_temperature(temperature=parsed_query['new_temp'])
+                        #respond(f'Changing temperature to {bot.get_temperature()}')
+                if parsed_query['new_temp'] == -1:
+                        respond(f"`!temp` only accepts values between 0 and 1. Using current value of `{actual_temp}`")
+                response, initial_message = get_llm_reply(bot, say, final_prompt,
+                                                          parsed_query['query'],
+                                                          to_all=True,
+                                                          thread_ts=thread_ts)
+                client.chat_update(
+                    channel=channel_id,
+                    ts=initial_message['ts'],
+                    text=response
+                )
+                # back to default temperature
+                bot.change_temperature(temperature=actual_temp)
+
+            except Exception as e:
+                logger.error(f"Error {e}")
+        else:
+            say("If you want to ask something, use command */ask*."
+                " If you want to include me on a discussion"
+                ", you have to mention me in a _thread_.")
+        #if len(text.split(' ')) > 1:
+        #    say(f"Hey, <@{user}>!, my name is <@{bot_user_id}>", thread_ts=thread_ts)
+
+    # @bot.event("message")
+    # def handle_message(body, say):
+    #     return
     
