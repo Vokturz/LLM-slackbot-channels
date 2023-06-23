3b37efa119cb3ddfe973ab125de1c60c2369d4de
Vokturz <victornavarrrokp47@gmail.com>
2023-06-23 09:08:59 -0400

    change thread QA to conversationalRetrievalChain

--- src/prompts.py
+++ src/prompts.py
@@ -10,20 +10,30 @@ DEFAULT_PROMPT = (
 
 THREAD_PROMPT = (
     f"{INITIAL_BOT_PROMPT}"
-    " Now, continue this conversation between users {users} and you with the name \"AI\"."
-    " If you don't know the answer, just say that you don't know, don't try to make up an answer."
-    "\n\n\"{conversation}\""
+    " Now, continue this conversation between users {users} and you with the"
+    " name \"AI\". If you don't know the answer, just say that you don't know,"
+    " don't try to make up an answer."
+    "\n\n\"{chat_history}\""
     "\nAI: "
 )
 
-THREAD_QA_PROMPT = (
+CONDENSE_QUESTION_PROMPT = (
     f"{INITIAL_BOT_PROMPT}"
-    " Use the following pieces of context to respond."
-    "\n\n{context}\n\n"
-    "This is the current conversation history between users {users} and you with the name \"AI\"."
-    "\n\n\"{conversation}\""
-    " If you don't know the answer, just say that you don't know, don't try to make up an answer."
-    "\nNow give a helpful answer to the followed user question."
-    "\n<@{user}> question: {question}"
-    "\nHelpful Answer:"
+    " Given the following conversation between users {users} and you with the"
+    " name \"AI\", and a follow up question, rephrase the follow up question"
+    " to be a standalone question, in its original language."
+    "\nChat History:\n{chat_history}"
+    "\nFollow Up Input: {question}"
+    "\nStandalone question:"
+)
+
+QA_PROMPT = (
+    f"{INITIAL_BOT_PROMPT}"
+    "Use the following pieces of context to answer the question at the end."
+    " If you don't know the answer, just say that you don't know, don't try"
+    "to make up an answer. "
+    "\nThis comes from a document about {extra_context}:\n"
+    "\n\n{context}\n"
+    "\nQuestion: {question}"
+    "\nHelpful Answer in the same language as question:"
 )
