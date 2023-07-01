fc21e95dd338cb2fd67066a73f04cba11b089758
Vokturz <victornavarrrokp47@gmail.com>
2023-07-01 02:24:58 -0400

    add introduction to home

--- webapp/00_🏠_Home.py
+++ webapp/00_🏠_Home.py
@@ -18,7 +18,22 @@ st.markdown(
 
 
 # Header
+col1, col2 = st.columns([0.5,1])
+col2.markdown('<img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Slack_icon_2019.svg" alt="drawing" width="80"/>',
+               unsafe_allow_html=True)
+col1.markdown("# LLM-slackbot-channels")
+st.markdown("""
+Welcome to LLM-slackbot-channels Web app!
+From here you can:
+### 1 Modify bot personality, instructions and temperature for a given channel
+1. Go to _#️⃣ Modify Channels_
+2. Select a channel. A form with the bot's info will appear
+3. Edit bot information as you wish
+### 2 Get and remove info about QA threads inside a channel
+1. Go to _❓ Threads QA Info_
+2. Select a channel. A list of QA threads will appear
+3. Select all QA threads you want to remove
+4. Remove QA threads. This will remove the vector data related to all selected QA threads.
+""")
 
-col1, col2, col3 = st.columns([4,1, 10])
-st.markdown("# Home")
 st.sidebar.markdown("# 🏠 Home")
